95683c3f8b0b50f034689843ff76f2be4083afc5
mateus senai <mateus_a_eugenio@estudante.sesisenai.org.br>
2026-04-30 14:37:46 -0300

    coloquei background e uma carta com as info do pokemon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+const [pokemons, setPokemons] = useState ({});
+const [loading, setLoading] = useState (true);
+const [error, setError] = useState (false);
+
+useEffect(() => {
+  const getData = async () => {
+    try {const res = await
+    axios.get('https://pokeapi.co/api/v2/pokemon/arceus');
+        setPokemons(res.data);
+        console.log('success:', res.data);
+        setLoading(false);
+    }
+    catch (err)  {
+      console.error ("erro ao carregar API", err);
+      setLoading(false)
+      setError (true)
+    }
+  };
+  getData();
+}, {})
+
+
+  if (loading) return <
+    div className='loader'> carregando pokedex...
+  </div>;
+
+  if (error) return <div className='error'>
+  ocorreu um erro inesperado
+  </div>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>poke card</h1>
+    <div className="pekemon-container">
+      <div className="pokemon-card">
+         <h3>{pokemons.name}</h3>
+         <h2 className='tipo-pokemon1'>{pokemons.types[0].type.name}</h2>
+         <img src={pokemons.sprites.front_default} alt={pokemons.name}
+         style={{ width: '150px', height: '150px' }} />
+          <h3>{pokemons.stats[0].stat.name}:
+              {pokemons.stats[0].base_stat}</h3>
+          <h3>{pokemons.stats[2].stat.name}: 
+              {pokemons.stats[1].base_stat}</h3>
+          <h3>{pokemons.stats[1].stat.name}: 
+              {pokemons.stats[1].base_stat}</h3>
+             </div>
+       </div>
+     </div>
+  
   );
 }
 
