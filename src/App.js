1e0e881496e6faa0fa2b2174bc459c118a3617c2
mateus senai <mateus_a_eugenio@estudante.sesisenai.org.br>
2026-05-06 17:14:24 -0300

    treinador
    pagina dois o treinador e o botao nao esta funcionando ainda

--- src/App.js
+++ src/App.js
@@ -1,58 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import pokemon from "./pokecard";
 
-function App() {
-const [pokemons, setPokemons] = useState ({});
-const [loading, setLoading] = useState (true);
-const [error, setError] = useState (false);
-
-useEffect(() => {
-  const getData = async () => {
-    try {const res = await
-    axios.get('https://pokeapi.co/api/v2/pokemon/totodile');
-        setPokemons(res.data);
-        console.log('success:', res.data);
-        setLoading(false);
-    }
-    catch (err)  {
-      console.error ("erro ao carregar API", err);
-      setLoading(false)
-      setError (true)
-    }
-  };
-  getData();
-}, {})
-
-
-  if (loading) return <
-    div className='loader'> carregando pokedex...
-  </div>;
-
-  if (error) return <div className='error'>
-  ocorreu um erro inesperado
-  </div>;
-
-  return (
+function App(){
+    return(
     <div className="App">
-    <h1>poke card</h1>
-    <div className="pekemon-container">
-      <div className="pokemon-card">
-         <h3>{pokemons.name}</h3>
-         <h2 className='tipo-pokemon1'>{pokemons.types[0].type.name}</h2>
-         <img src={pokemons.sprites.front_default} alt={pokemons.name}
-         style={{ width: '150px', height: '150px' }} />
-          <h3>{pokemons.stats[0].stat.name}:
-              {pokemons.stats[0].base_stat}</h3>
-          <h3>{pokemons.stats[2].stat.name}: 
-              {pokemons.stats[1].base_stat}</h3>
-          <h3>{pokemons.stats[1].stat.name}: 
-              {pokemons.stats[1].base_stat}</h3>
-             </div>
-       </div>
-     </div>
-  
-  );
+    <h1>Treinador</h1>
+    <h2> Avatar</h2>
+    <img className='perfil'
+    src="https://imgs.search.brave.com/QJ5kdi9zcIiJtd-yhbawr01ZCaa4zDwTUNirFn-u1-0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMud2lraWEubm9j/b29raWUubmV0L3Bv/a2Vtb24vaW1hZ2Vz/LzMvM2YvTl9CVy5w/bmcvcmV2aXNpb24v/bGF0ZXN0P2NiPTIw/MTEwMjEwMjE1OTMz"
+    style={{ width: '150px', height: '150px' }}/>
+    <h3>nome: Mateus A Eugenio</h3>
+    <h3>idade: 18</h3>
+    <h3>nickname: pal4din</h3>
+    <h3>pokemon: 1</h3>
+    <h3>0 bagdges</h3>
+   <button></button>
+   
+    </div>
+       /* 
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<pokemon/>}/>
+            </Routes>
+        </BrowserRouter>*/
+    )
 }
 
 export default App;
---
+++ src/pokecard.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './pokebonito.css';
+
+function App() {
+const [pokemons, setPokemons] = useState ({});
+const [loading, setLoading] = useState (true);
+const [error, setError] = useState (false);
+
+useEffect(() => {
+  const getData = async () => {
+    try {const res = await
+    axios.get('https://pokeapi.co/api/v2/pokemon/totodile');
+        setPokemons(res.data);
+        console.log('success:', res.data);
+        setLoading(false);
+    }
+    catch (err)  {
+      console.error ("erro ao carregar API", err);
+      setLoading(false)
+      setError (true)
+    }
+  };
+  getData();
+}, {})
+
+
+  if (loading) return <
+    div className='loader'> carregando pokedex...
+  </div>;
+
+  if (error) return <div className='error'>
+  ocorreu um erro inesperado
+  </div>;
+
+  return (
+    <div className="App">
+    <h1>poke card</h1>
+    <div className="pekemon-container">
+      <div className="pokemon-card">
+         <h3>{pokemons.name}</h3>
+         <h2 className='tipo-pokemon1'>{pokemons.types[0].type.name}</h2>
+         <img src={pokemons.sprites.front_default} alt={pokemons.name}
+         style={{ width: '150px', height: '150px' }} />
+          <h3>{pokemons.stats[0].stat.name}:
+              {pokemons.stats[0].base_stat}</h3>
+          <h3>{pokemons.stats[2].stat.name}: 
+              {pokemons.stats[1].base_stat}</h3>
+          <h3>{pokemons.stats[1].stat.name}: 
+              {pokemons.stats[1].base_stat}</h3>
+             </div>
+       </div>
+     </div>
+  
+  );
+}
+
+export default App;
